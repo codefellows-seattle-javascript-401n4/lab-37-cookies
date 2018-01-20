00dfcf6bf52449034fff2b08c684e103098ef0db
jrzollin <jrzollin@yahoo.com>
2018-01-20 15:02:09 -0800

    init off cookie working

--- lab-james/client/src/components/app.js
+++ lab-james/client/src/components/app.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import Header from './header.js';
 import Nav from './nav.js';
 import LogIn from './login/log-in.js';
 import Home from './home/home.js';
+import {authInit} from './auth/auth-actions.js';
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
+  }
+
+  componentWillMount(){
+    this.props.handleAuthInit();
   }
 
   render(){
@@ -26,4 +32,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+const mapDispatchToProps = (dispatch, getState) => ({
+  handleAuthInit: () => dispatch(authInit())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- lab-james/server/routes/auth-routes.js
+++ lab-james/server/routes/auth-routes.js
@@ -3,6 +3,7 @@
 const User = require('../models/user.js');
 const basicHTTP = require('../lib/middleware/basicHTTP.js');
 const jsonParser = require('body-parser').json();
+const bearerAuth = require('../lib/middleware/bearer-auth.js');
 
 const authRouter = module.exports = require('express').Router();
 
@@ -32,7 +33,7 @@ authRouter.post('/createUser', jsonParser, (req, res, next) => {
       user.save()
         .then(user => {
           let token = user.generateToken();
-          res.cookie('auth', token);
+          res.cookie('auth', token, {maxAge: 900000});
           res.send({user, token});
         })
         .catch(err => {
@@ -54,12 +55,24 @@ authRouter.get('/findUser', basicHTTP, (req, res, next) => {
       user.checkPass(req.auth.password)
         .then(user => {
           let token = user.generateToken();
-          res.cookie('auth', token);
+          res.cookie('auth', token, {maxAge: 900000});
           res.send({user, token});
         })
         .catch( () => {
           next({statusCode: 401, message: 'Invalid password'});
         });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+authRouter.get('/auth/findUser', bearerAuth, (req, res, next) => {
+  User.findOne({_id: req.user._id})
+    .then(user => {
+      let token = user.generateToken();
+      res.cookie('auth', token, {maxAge: 900000});
+      res.send({user, token});
     })
     .catch(err => {
       next(err);
